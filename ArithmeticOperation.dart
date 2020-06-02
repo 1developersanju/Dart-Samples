ba01137574d390d62c6aff67cfa068250bb9cca0
DeveloperSanju <sanjuwings04@gmail.com>
2020-06-02 23:14:00 +0530

    Initial files

---
+++ ArithmeticOperation.dart
@@ -0,0 +1,8 @@
+void main(){
+  int a = 55;
+  int b = 6;
+  num c = a - b;
+  print("a - b:");
+  print(c);
+  
+}
---
+++ MethodOverriding.dart
@@ -0,0 +1,26 @@
+void main(){
+  var lessie = Pet();
+  lessie.name = "Lessie";
+  lessie.hasBirthday();
+}
+
+class Pet extends Animal{
+  String name;
+  
+  void hasBirthday(){
+    super.hasBirthday();
+    print(name);
+  }
+}
+
+class WildlifeAnimal extends Animal{
+  
+}
+
+class Animal{
+  int age;
+  
+  void hasBirthday(){
+    print("Happy Birthday");
+  }
+}

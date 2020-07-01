fc90bfe196785ef927fb86ca7591dc4779f9e556
DeveloperSanju <sanjuwings04@gmail.com>
2020-07-02 02:32:47 +0530

    commit of 128 samples

---
+++ 56 Class-1.dart
@@ -0,0 +1,13 @@
+void main() { 
+   Avenger c= new Avenger(); 
+   c.shows(); 
+}  
+class Avenger {  
+   // field 
+   String favouriteAvenger = "IRON MAN";  
+   
+   // function 
+   void shows() { 
+      print(favouriteAvenger); 
+   } 
+}

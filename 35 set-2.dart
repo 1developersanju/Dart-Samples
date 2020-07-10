e21fef673e8196e4bb2104e89f03ee943d3e32aa
DeveloperSanju <sanjuwings04@gmail.com>
2020-07-10 20:11:36 +0530

    150 examples

--- 35 set-2.dart
+++ 35 set-2.dart
@@ -20,3 +20,22 @@ main() {
   print(medals.lookup("platinum"));
   print(medals.difference(meals));
 }
+-------------------------------------------------------------------------------
+
+
+void main() { 
+   Set <int>numberSet = new  Set<int>(); 
+   numberSet.add(100); 
+   numberSet.add(20); 
+   numberSet.add(5); 
+   numberSet.add(60);
+   numberSet.add(70); 
+  
+  
+   CompilationError:
+   print("Default implementation : ${numberSet.runtimeType}");  
+   
+   for(var no in numberSet) { 
+      print(no); 
+   } 
+} 
